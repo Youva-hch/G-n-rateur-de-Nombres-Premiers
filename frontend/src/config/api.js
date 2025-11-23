37817c115917f9a3f219d87e1d416556b5cacf98
Noa-Miramont <noamiramont@gmail.com>
2025-11-23 20:25:57 +0100

    Nettoyage: ne garder que le dossier G-n-rateur-de-Nombres-Premiers-backend

--- frontend/src/config/api.js
+++
@@ -1,7 +0,0 @@
-export const API_BASE_URL = '/api'
-
-export const API_ENDPOINTS = {
-  UPLOAD: `${API_BASE_URL}/upload`,
-  DOWNLOAD: `${API_BASE_URL}/download`,
-  FILES: `${API_BASE_URL}/files`
-} 
